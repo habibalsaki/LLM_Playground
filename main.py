77f7a8e6165f0ce8648ef53fc9030bb7d8f341c0
habibalsaki <habibalsaki@gmail.com>
2025-06-12 01:45:15 +0600

    inital notebook with prompt

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 def main():
+    key = os.getenv("api_key")
     print("Hello from zoomcamp!")
+    print(key)
 
 
 if __name__ == "__main__":
